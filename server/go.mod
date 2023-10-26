83cf7069ed992c1489263b43fd354adbe854eb17
Daaaai0809 <dai.tsuruga@80and.co>
2023-10-26 13:58:55 +0900

    feat: modify model/user.go + infra/conn.go

--- server/go.mod
+++ server/go.mod
@@ -4,7 +4,6 @@ go 1.20
 
 require (
 	github.com/go-sql-driver/mysql v1.7.1
-	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.11.2
 	github.com/uptrace/bun v1.1.16
 	github.com/uptrace/bun/dialect/mysqldialect v1.1.16
--- server/infra/conn.go
+++ server/infra/conn.go
@@ -12,19 +12,20 @@ const (
 	driver = "mysql"
 	port = "3306"
 	net = "tcp"
+	host = "database"
 )
 
 func ConnectDB() (*sql.DB, error) {
 	c := mysql.Config{
 		User: os.Getenv("MYSQL_USER"),
-		Passwd: os.Getenv("MYSQL_ROOT_PASSWORD"),
+		Passwd: os.Getenv("MYSQL_PASSWORD"),
 		Net: net,
-		Addr: fmt.Sprintf("%s:%s", os.Getenv("MYSQL_HOST"), port),
+		Addr: fmt.Sprintf("%s:%s", host, port),
 		DBName: os.Getenv("MYSQL_DATABASE"),
 		AllowNativePasswords: true,
 		ParseTime: true,
 	}
-	
+
 	db, err := sql.Open(driver, c.FormatDSN())
 	if err != nil {
 		return nil, err
