915294cdc85e33ab1f05cd963831821a76abfebc
sor4chi <sorakawa0218@gmail.com>
2023-10-26 12:26:46 +0900

    feat: control loading state inside of onboarding page

--- client/src/features/onboarding/page.tsx
+++ client/src/features/onboarding/page.tsx
@@ -1,6 +1,18 @@
+import { useContext, useEffect } from "react";
 import { styles } from "./page.css";
+import { LoadingContext } from "@/providers/loading";
 
 export const OnboardingPage = () => {
+  const { setIsLoading } = useContext(LoadingContext);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, [setIsLoading]);
+
   return (
     <div className={styles.container}>
       <img src="/maximum.svg" alt="Maximum" width="300" height="100" />
