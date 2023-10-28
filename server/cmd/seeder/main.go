d59805fd652b4e5b655adb7ba97f45e6bd5480c2
Daaaai0809 <dai.tsuruga@80and.co>
2023-10-28 18:56:58 +0900

    feat: add seeder.sh + seeder/main.go

---
+++ server/cmd/seeder/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/mysqldialect"
+	infra "github.com/saitamau-maximum/meline/infra/mysql"
+	model "github.com/saitamau-maximum/meline/usecase/model"
+)
+
+const (
+	host = "localhost"
+)
+
+var (
+	users = []*model.User{
+		{
+			GithubID: "sample1",
+			Name: "name_1",
+		},
+		{
+			GithubID: "sample2",
+			Name: "name_2",
+		},
+		{
+			GithubID: "sample3",
+			Name: "name_3",
+		},
+	}		
+)
+
+func main() {
+	db, err := infra.ConnectDB(host)
+	if err != nil {
+		log.Printf("failed to connect db: %v", err)
+		return
+	}
+
+	bunDB := bun.NewDB(db, mysqldialect.New())
+	defer bunDB.Close()
+
+	if err := seeds(context.Background(), bunDB); err != nil {
+		log.Printf("failed to seed: %v", err)
+		return
+	}
+
+	log.Printf("seeding is done")
+}
+
+func seeds(ctx context.Context, db *bun.DB) error {
+	if _, err := db.NewInsert().Model(&users).Exec(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
