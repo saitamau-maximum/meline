faf1248487f3da389915adb843523c6b20db4150
sor4chi <sorakawa0218@gmail.com>
2023-11-16 10:39:55 +0900

    fix: ImageURLがuser entityになかったので追加

--- server/domain/entity/user.go
+++ server/domain/entity/user.go
@@ -5,22 +5,23 @@ import (
 )
 
 type User struct {
-	ID uint64 `json:"id"`
-	ProviderID string `json:"provider_id"`
-	Name string `json:"name"`
-	ImageURL string `json:"image_url"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at"`
+	ID         uint64    `json:"id"`
+	ProviderID string    `json:"provider_id"`
+	Name       string    `json:"name"`
+	ImageURL   string    `json:"image_url"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+	DeletedAt  time.Time `json:"deleted_at"`
 }
 
-func NewUserEntity(id uint64, providerID, name string, createdAt, updatedAt, deletedAt time.Time) *User {
+func NewUserEntity(id uint64, providerID, name, ImageURL string, createdAt, updatedAt, deletedAt time.Time) *User {
 	return &User{
-		ID: id,
+		ID:         id,
 		ProviderID: providerID,
-		Name: name,
-		CreatedAt: createdAt,
-		UpdatedAt: updatedAt,
-		DeletedAt: deletedAt,
+		Name:       name,
+		ImageURL:   ImageURL,
+		CreatedAt:  createdAt,
+		UpdatedAt:  updatedAt,
+		DeletedAt:  deletedAt,
 	}
 }
--- server/models/user.go
+++ server/models/user.go
@@ -7,15 +7,15 @@ import (
 )
 
 type User struct {
-	ID uint64 `bun:"id,pk,autoincrement"`
-	ProviderID string `bun:"provider_id,unique"`
-	Name string `bun:"name,notnull"`
-	ImageURL string `bun:"image_url"`
-	CreatedAt time.Time `bun:"created_at,notnull,default:current_timestamp"`
-	UpdatedAt time.Time `bun:"updated_at,notnull,default:current_timestamp"`
-	DeletedAt time.Time `bun:"deleted_at,default:null"`
+	ID         uint64    `bun:"id,pk,autoincrement"`
+	ProviderID string    `bun:"provider_id,unique"`
+	Name       string    `bun:"name,notnull"`
+	ImageURL   string    `bun:"image_url"`
+	CreatedAt  time.Time `bun:"created_at,notnull,default:current_timestamp"`
+	UpdatedAt  time.Time `bun:"updated_at,notnull,default:current_timestamp"`
+	DeletedAt  time.Time `bun:"deleted_at,default:null"`
 }
 
 func (u *User) ToUserEntity() *entity.User {
-	return entity.NewUserEntity(u.ID, u.ProviderID, u.Name, u.CreatedAt, u.UpdatedAt, u.DeletedAt)
+	return entity.NewUserEntity(u.ID, u.ProviderID, u.Name, u.ImageURL, u.CreatedAt, u.UpdatedAt, u.DeletedAt)
 }
