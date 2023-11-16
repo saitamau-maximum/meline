031e50cf3499f5bbdb7a789fcfe00a6ca0ddbde5
sor4chi <sorakawa0218@gmail.com>
2023-11-16 12:58:31 +0900

    docs: color tokensのstorybookを完成

--- client/src/styles/__stories__/Theme.tsx
+++ client/src/styles/__stories__/Theme.tsx
@@ -1,14 +1,28 @@
 import "../global.css";
+import { vars } from "../theme.css";
+
+const styles = {
+  background: vars.color.gray[1],
+  color: vars.color.gray[12],
+  padding: "16px",
+  border: `1px solid ${vars.color.gray[6]}`,
+  borderRadius: "8px",
+  width: "fit-content",
+};
 
 const LightWrapper = ({ children }: { children: React.ReactNode }) => (
-    <div className="light">{children}</div>
+  <div className="light" style={styles}>
+    {children}
+  </div>
 );
 
 const DarkWrapper = ({ children }: { children: React.ReactNode }) => (
-    <div className="dark">{children}</div>
+  <div className="dark" style={styles}>
+    {children}
+  </div>
 );
 
 export const Theme = {
-    Light: LightWrapper,
-    Dark: DarkWrapper,
+  Light: LightWrapper,
+  Dark: DarkWrapper,
 };
--- client/src/styles/__stories__/color-tokens.stories.tsx
+++ client/src/styles/__stories__/color-tokens.stories.tsx
@@ -3,55 +3,63 @@ import { vars } from "../theme.css";
 import { Theme } from "./Theme";
 
 const meta = {
-    title: "Styles/Color Tokens",
+  title: "Styles/Color Tokens",
 } satisfies Meta;
 
 export default meta;
 
-export const ColorTokensLight = () => (
-    <Theme.Light>
-        <h1>Color Tokens</h1>
-        <ul>
-            {Object.entries(vars.color.gray).map(([key, value]) => (
-                <>
-                    <li key={key}>
-                        <code>{key}</code>: <code>{value}</code>
-                        <div
-                            key={key}
-                            style={{
-                                backgroundColor: value,
-                                height: "5rem",
-                                width: "5rem",
-                                borderRadius: "50%",
-                            }}
-                        />
-                    </li>
-                </>
-            ))}
-        </ul>
-    </Theme.Light>
+const ColorTokens = () => (
+  <>
+    <h1>Color Tokens</h1>
+    <div
+      style={{
+        display: "flex",
+        gap: "32px",
+        padding: "32px",
+      }}
+    >
+      {Object.entries(vars.color.gray).map(([key, value]) => (
+        <div
+          key={key}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "12px",
+          }}
+        >
+          <div
+            key={key}
+            style={{
+              backgroundColor: value,
+              height: "48px",
+              width: "48px",
+              borderRadius: "50%",
+              border: `1px solid ${vars.color.gray[6]}`,
+            }}
+          />
+          <code
+            style={{
+              fontSize: "0.75rem",
+              color: vars.color.gray[12],
+            }}
+          >
+            Gray {key}
+          </code>
+        </div>
+      ))}
+    </div>
+  </>
 );
 
-export const ColorTokensDark = () => (
-    <Theme.Dark>
-        <h1>Color Tokens</h1>
-        <ul>
-            {Object.entries(vars.color.gray).map(([key, value]) => (
-                <>
-                    <li key={key}>
-                        <code>{key}</code>: <code>{value}</code>
-                        <div
-                            key={key}
-                            style={{
-                                backgroundColor: value,
-                                height: "5rem",
-                                width: "5rem",
-                                borderRadius: "50%",
-                            }}
-                        />
-                    </li>
-                </>
-            ))}
-        </ul>
-    </Theme.Dark>
+export const Light = () => (
+  <Theme.Light>
+    <ColorTokens />
+  </Theme.Light>
+);
+
+export const Dark = () => (
+  <Theme.Dark>
+    <ColorTokens />
+  </Theme.Dark>
 );
