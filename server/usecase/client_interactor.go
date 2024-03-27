c284f9c2914c26a9a41c0c942a851e29a85fe158
Daaaai0809 <dai.tsuruga@80and.co>
2024-03-28 01:06:53 +0900

    refactor: read loop 削除

--- server/usecase/client_interactor.go
+++ server/usecase/client_interactor.go
@@ -10,7 +10,6 @@ import (
 
 type IClientInteractor interface {
 	Disconnect(client *entity.Client)
-	ReadLoop(ctx context.Context, broadCast chan<- []byte, channelId chan<- uint64, client *entity.Client) error
 	WriteLoop(ctx context.Context, client *entity.Client) error
 }
 
@@ -22,31 +21,6 @@ func NewClientInteractor() *ClientInteractor {
 
 func (c *ClientInteractor) Disconnect(client *entity.Client) {
 	client.Ws.Close()
-}
-
-func (c *ClientInteractor) ReadLoop(ctx context.Context, broadCast chan<- []byte, channelId chan<- uint64, client *entity.Client) error {
-	defer func() {
-		c.Disconnect(client)
-	}()
-
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		default:
-			{
-				_, message, err := client.Ws.ReadMessage()
-				if err != nil {
-					if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-						return fmt.Errorf("websocket: %w", err)
-					}
-				}
-
-				broadCast <- message
-				channelId <- client.ChannelID
-			}
-		}
-	}
 }
 
 func (c *ClientInteractor) WriteLoop(ctx context.Context, client *entity.Client) error {
