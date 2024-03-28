48709720d45051d1dc015b52cf854b254987091a
Daaaai0809 <dai.tsuruga@80and.co>
2024-03-29 02:09:52 +0900

    feat: add infra

---
+++ server/infra/mysql/message_repository.go
@@ -0,0 +1,81 @@
+package mysql
+
+import (
+	"context"
+	"sync"
+
+	"github.com/saitamau-maximum/meline/domain/repository"
+	"github.com/saitamau-maximum/meline/models"
+	"github.com/uptrace/bun"
+)
+
+type messageRepository struct {
+	db *bun.DB
+	mu sync.RWMutex
+}
+
+func NewMessageRepository(db *bun.DB) repository.IMessageRepository {
+	return &messageRepository{db: db}
+}
+
+func (r *messageRepository) FindByChannelID(ctx context.Context, channelID uint64) ([]*model.Message, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	var messages []*model.Message
+	err := r.db.NewSelect().Model(&messages).Where("channel_id = ?", channelID).Relation("ReplyToMessage").Relation("ReplyToMessage.User").Relation("User").Order("created_at ASC").Scan(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return messages, nil
+}
+
+func (r *messageRepository) FindByID(ctx context.Context, id string) (*model.Message, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	message := &model.Message{}
+	err := r.db.NewSelect().Model(message).Where("id = ?", id).Relation("ReplyToMessage").Relation("ReplyToMessage.User").Scan(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return message, nil
+}
+
+func (r *messageRepository) Create(ctx context.Context, message *model.Message) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	_, err := r.db.NewInsert().Model(message).Exec(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *messageRepository) Update(ctx context.Context, message *model.Message) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	_, err := r.db.NewUpdate().Model(message).Where("id = ?", message.ID).Exec(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *messageRepository) Delete(ctx context.Context, id string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	_, err := r.db.NewDelete().Model(&model.Message{}).Where("id = ?", id).Exec(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
