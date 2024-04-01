334e8f52b2947171c539bf3bf631560f78d5c871
Daaaai0809 <dai.tsuruga@80and.co>
2024-04-01 19:52:13 +0900

    refactor: remove unused entities

--- server/infra/mysql/channel_repository.go
+++ server/infra/mysql/channel_repository.go
@@ -30,21 +30,65 @@ func (r *ChannelRepository) FindByID(ctx context.Context, id uint64) (*model.Cha
 	return &channel, nil
 }
 
-func (r *ChannelRepository) Create(ctx context.Context, channel *model.Channel) (uint64, error) {
+func (r *ChannelRepository) Create(ctx context.Context, channel *model.Channel, userId uint64) error {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
 	res, err := r.db.NewInsert().Model(channel).Exec(ctx)
 	if err != nil {
-		return 0, err
+		return err
 	}
 
 	id, err := res.LastInsertId()
 	if err != nil {
-		return 0, err
+		return err
 	}
 
-	return uint64(id), err
+	channelToUsers := &model.ChannelUsers{
+		ChannelID: uint64(id),
+		UserID:    userId,
+	}
+
+	if _, err := r.db.NewInsert().Model(channelToUsers).Exec(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *ChannelRepository) CreateChildChannel(ctx context.Context, channel *model.Channel, parentChannelID uint64, userId uint64) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	res, err := r.db.NewInsert().Model(channel).Exec(ctx)
+	if err != nil {
+		return err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	channelToUsers := &model.ChannelUsers{
+		ChannelID: uint64(id),
+		UserID:    userId,
+	}
+
+	if _, err := r.db.NewInsert().Model(channelToUsers).Exec(ctx); err != nil {
+		return err
+	}
+
+	channelToChannels := &model.ChannelToChannels{
+		ParentChannelID: parentChannelID,
+		ChildChannelID:  uint64(id),
+	}
+
+	if _, err := r.db.NewInsert().Model(channelToChannels).Exec(ctx); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (r *ChannelRepository) Update(ctx context.Context, channel *model.Channel) error {
@@ -63,6 +107,41 @@ func (r *ChannelRepository) Delete(ctx context.Context, id uint64) error {
 	defer r.mu.Unlock()
 
 	if _, err := r.db.NewDelete().Model(&model.Channel{}).Where("id = ?", id).Exec(ctx); err != nil {
+		return err
+	}
+
+	if _, err := r.db.NewDelete().Model(&model.ChannelUsers{}).Where("channel_id = ?", id).Exec(ctx); err != nil {
+		return err
+	}
+
+	if _, err := r.db.NewDelete().Model(&model.ChannelToChannels{}).WhereOr("parent_channel_id = ?", id).WhereOr("child_channel_id = ?", id).Exec(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *ChannelRepository) JoinChannel(ctx context.Context, channelID uint64, userID uint64) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	channelToUsers := &model.ChannelUsers{
+		ChannelID: channelID,
+		UserID:    userID,
+	}
+
+	if _, err := r.db.NewInsert().Model(channelToUsers).Exec(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *ChannelRepository) LeaveChannel(ctx context.Context, channelID uint64, userID uint64) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if _, err := r.db.NewDelete().Model(&model.ChannelUsers{}).Where("channel_id = ?", channelID).Where("user_id = ?", userID).Exec(ctx); err != nil {
 		return err
 	}
 
--- server/models/channel_users.go
+++ server/models/channel_users.go
@@ -2,8 +2,6 @@ package model
 
 import (
 	"time"
-
-	"github.com/saitamau-maximum/meline/domain/entity"
 )
 
 type ChannelUsers struct {
@@ -12,8 +10,4 @@ type ChannelUsers struct {
 	ChannelID uint64    `bun:"channel_id,pk"`
 	Channel   *Channel  `bun:"rel:belongs-to,join:channel_id=id"`
 	JoinedAt  time.Time `bun:"joined_at,notnull,default:current_timestamp"`
-}
-
-func (cu *ChannelUsers) ToChannelUsersEntity() *entity.ChannelUsers {
-	return entity.NewChannelUsersEntity(cu.UserID, cu.User.ToUserEntity(), cu.ChannelID, cu.Channel.ToChannelEntity(), cu.JoinedAt)
 }
